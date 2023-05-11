5e60b83f46d44088e674ac5587703d2819ad061d
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-05-11 18:18:13 +0700

    [FIX] refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,43 @@
 import Link from "next/link";
+import style from "./home.module.css";
 
 export default function Home() {
   return (
-    <main className="pt-32 px-8 flex flex-col gap-4">
-      <h4 className="text-lg font-bold">Welcome Bubugu..!</h4>
-      <h1 className="text-4xl font-extrabold">BE WISE TO MANAGE YOUR MONEY</h1>
-      <div>
-        <p className="text-sm">Bubuget will help you to manage your money.</p>
-        <p className="text-sm">Simple way that will save your future budget.</p>
+    <main>
+      <div className={style.splash}>
+        <div className={style.splash1}>
+          <h4>WELCOME BUBUGU..!</h4>
+        </div>
+        <div className={style.splash2}>
+          <h1>
+            BE WISE TO <br />
+            MANAGE <br />
+            YOUR MONEY
+          </h1>
+        </div>
+        <div className={style.splash3}>
+          Bubuget will help you to <br />
+          manage your money. <br />
+          It{"'"}s a simple way to save <br />
+          your future budget.
+        </div>
       </div>
-      <div className="flex gap-4 mt-8">
-        <button className="py-2 px-8 rounded-xl bg-emerald-600/60 text-emerald-950 font-black text-sm">
-          <Link href="/auth/signup">Sign Up</Link>
-        </button>
-        <button className="py-2 px-8 rounded-xl bg-emerald-600/60 text-emerald-950 font-black text-sm">
-          <Link href="/auth/login">Login</Link>
-        </button>
-      </div>
+
+      <table cellPadding="5" width="300" className={style.splash}>
+        <tbody>
+          <tr>
+            <td></td>
+
+            <td className={style.in}>
+              <Link href="auth/signup">Sign Up</Link>
+            </td>
+            <td></td>
+            <td className={style.in}>
+              <Link href="auth/login">Log In</Link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 }
